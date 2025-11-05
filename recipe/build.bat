895192716d0984201f421fa5fadae69b5b08f66f
Johan Mabille <johan.mabille@gmail.com>
2025-11-05 23:35:01 +0100

    Fixed build.bat

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,4 +1,9 @@
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DXEXTRA_JUPYTER_DATA_DIR=%PREFIX%\\share\\jupyter %SRC_DIR%
+REM For an unknwon reason cmake fails to parse correctly XEXTRA_JUPYTER_DATA_DIR=%PREFIX%\\share\\jupyter, so let's not use it and copy te kernelspec after
+cmake -G "NMake Makefiles" ^
+  -D CMAKE_BUILD_TYPE=Release ^
+  -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  %SRC_DIR%
+
 if errorlevel 1 exit 1
 
 nmake
@@ -6,3 +11,8 @@ if errorlevel 1 exit 1
 
 nmake install
 if errorlevel 1 exit 1
+
+REM Copying kernelspec to the general prefix for Jupyter to pick it up.
+
+md %PREFIX%\share\jupyter\kernels\xlua
+xcopy %LIBRARY_PREFIX%\share\jupyter\kernels\xlua %PREFIX%\share\jupyter\kernels\xlua /F /Y
